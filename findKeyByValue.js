36d0ae1a7de0d257254207cd120b8dd571c5ae39
Tony Gibbs <tony@tgmedia.ca>
2021-11-15 22:47:33 +0000

    Updated findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -25,7 +25,7 @@ const findKeyByValue = function(obj, srchVal) {
     // Scan the object and return the first key which contains the given value. 
     // If no key with that given value is found, then return undefined.
     retVal = undefined;
-    console.log("");
+
     objKeys = Object.keys(obj);
 
     for(val of objKeys){
