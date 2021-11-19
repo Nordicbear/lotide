4961ca5e4341770afbdead1ace348fab466a3c90
Tony Gibbs <tony@tgmedia.ca>
2021-11-19 21:07:55 +0000

    Set up testing for middle.js with middleTest.js

--- middle.js
+++ middle.js
@@ -1,40 +1,14 @@
-let arr1 = new Array();
-let arr2 = new Array();
+const assertArraysEqual = require('./assertArraysEqual');
 
 let first = 0;
 let second = 0;
-let newVal = 0;
-
-const eqArrays = function (arr1, arr2) {
-    let retval = true;
-
-    if(arr1.length == arr2.length) {
-        retval = false;
-    } else {
-        for (let i=0; i < arr1.length; i++) {
-            if (arr1[i] !== arr2[i]) {
-                retval = false;
-            }
-        }
-    }
-    return retval;
-}
-
-const assertArraysEqual  = function (a1, a2) {
-    if (eqArrays(a1, a2)) {
-        console.log("✅ Assertion Passed: " + a1 + " === " + a2);
-    } else
-    {
-        console.log("🛑 Assertion Failed: " + a1 + " !== " + a2);
-    }
-}
 
 const middle = function(testArr) {
     const newArr = [];
     //first thing we should do is check for how many elements are in the array
     if (testArr.length < 3) {
         //if the array has 1 or 2 elemnts we return an empty array
-        newArr.lenth = 0;
+        newArr.length = 0;
       } else
     //now check if the array has an odd/even number of elements
     if ((testArr.length % 2) == 1) {
@@ -63,3 +37,5 @@ console.log(middle([1, 2]));
 console.log(middle([1, 2, 3]));
 console.log(middle([1, 2, 3, 4]));
 console.log(middle([1, 2, 3, 4, 5]));
+
+module.exports = middle;
---
+++ middleTest.js
@@ -0,0 +1,6 @@
+const middle = require('../middle');
+
+console.log(middle([1, 2]));
+console.log(middle([1, 2, 3]));
+console.log(middle([1, 2, 3, 4]));
+console.log(middle([1, 2, 3, 4, 5]));
