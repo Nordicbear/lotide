be1a4690cea1623b8f7a177a1a9e267b6c29e95b
Tony Gibbs <tony@tgmedia.ca>
2021-11-19 19:27:54 +0000

    Updated assertEqaul.js and head.js

--- assertEqual.js
+++ assertEqual.js
@@ -4,15 +4,18 @@ let actual ="", expected = "";
 const assertEqual = function(actual, expected) {
   // console.assert(actual === expected, "🛑 " + actual + " != " + expected);
   if (actual === expected) {
-      console.log("✅ Assertin Passed: " + expected + " === " + actual);
+    console.log("✅ Assertin Passed: " + expected + " === " + actual);
   } else
   {
     console.log("🛑 Assertin Failed: " + expected + " !== " + actual);
   }
 }
 
+/*
 // TEST CODE
 assertEqual("The Cat", "The Cat");
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 2);
 assertEqual(1, 1);
+*/
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,26 +1,9 @@
-// FUNCTION IMPLEMENTATION
-let actual ="", expected = "";
-let values = [];
+const assertEqual = require('./assertEqual');
 
-const assertEqual = function(actual, expected) {
-  // console.assert(actual === expected, "🛑 " + actual + " != " + expected);
-  if (actual === expected) {
-      console.log("✅ Assertin Passed: " + expected + " === " + actual);
-  } else
-  {
-    console.log("🛑 Assertin Failed: " + expected + " !== " + actual);
-  }
-}
+let values = [];
 
 const head = function(values) {
     return(values[0]);
 }
 
-// TEST CODE
-assertEqual("The Cat", "The Cat");
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 2);
-assertEqual(1, 1);
-
-assertEqual(head(["Hi", "Bye", "Hello"]), "Hi");
-assertEqual(head([5, 10, 15]), 10);
+module.exports = head;
