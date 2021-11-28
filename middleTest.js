de5856295e7ead6da3c890c9af2ddb84b0197c6e
Tony Gibbs <tony@tgmedia.ca>
2021-11-28 18:11:39 +0000

    Added middleTest.js

--- middleTest.js
+++ middleTest.js
@@ -1,6 +1,28 @@
 const middle = require('../middle');
+const assert = require('chai').assert;
 
+describe("#head", () => {
+    it("returns [] for [1]", () => {
+        assert.deepEqual(middle([1]), []);
+    });
+
+    it("returns [2] for [1, 2, 3]", () => {
+        assert.deepEqual(middle([1, 2, 3]), [2]);
+    });
+
+    it("returns [2, 3] for [1, 2, 3, 4]", () => {
+        assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+    });
+
+    it("returns [3] for [1, 2, 3, 4, 5]", () => {
+        assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+    });
+
+});
+
+/*
 console.log(middle([1, 2]));
 console.log(middle([1, 2, 3]));
 console.log(middle([1, 2, 3, 4]));
 console.log(middle([1, 2, 3, 4, 5]));
+*/
